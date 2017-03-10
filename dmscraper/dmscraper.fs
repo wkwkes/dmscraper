b7f7c1ffcca86029a86c18fb2b6ee07eb894ea89
wkwkes <oshi.hiroki@gmail.com>
2017-03-10 15:11:31 +0900

    tmp

--- dmscraper/dmscraper.fs
+++ dmscraper/dmscraper.fs
@@ -1,6 +1,21 @@
-module dmscraper
+module DmScraper
+
+open FSharp.Data
+
+// let url = "https://elk.bookmeter.com/users/580549/books/read"
+
+type Books = HtmlProvider< "https://elk.bookmeter.com/users/580549/books/read" >
+let data = Books.Load("https://elk.bookmeter.com/users/580549/books/read")
+
+let bookList = data.Html.Descendants["div"] 
+               |>  Seq.filter (fun (x:HtmlNode) -> x.HasAttribute("class", "book__detail"))
+               
+// bookList |> Seq.iter (fun x -> x.ToString() |> printfn "--\n%s" )
+
+let 
+
 
 [<EntryPoint>]
-let main argv =
-    printfn "%A" argv
+let main argv = 
+    // Seq.iter (fun (x, y) -> printfn "%A %A" x y) b 
     0 // return an integer exit code
