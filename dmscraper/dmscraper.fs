cb8631c2b34b3afa9cda20b7ab3b029966554419
wkwkes <oshi.hiroki@gmail.com>
2017-03-10 17:45:13 +0900

    tmp

--- dmscraper/dmscraper.fs
+++ dmscraper/dmscraper.fs
@@ -3,8 +3,8 @@ module DmScraper
 open FSharp.Data
 open System.Text.RegularExpressions
 
-type BookHtml = HtmlProvider< "https://elk.bookmeter.com/users/580549/books/read" >
-type BookJson = JsonProvider<""" { "date":[2017, 3, 8], "title":"羆嵐", "author":"吉村 昭", "page":226 } """>
+// type BookHtml = HtmlProvider< "https://elk.bookmeter.com/users/580549/books/read" >
+type BookJson = JsonProvider<""" { "date":[2017, 3, 8], "title":"羆嵐", "author":"吉村 昭", "page":226, "comment":"おもしろかった" } """>
 
 let parseDate date =
     let (|Regex|_|) pattern input =
@@ -12,10 +12,22 @@ let parseDate date =
         if m.Success then Some(List.tail [ for g in m.Groups -> g.Value ])
         else None
     match date with
-    | Regex @"([0-9]{4})年([0-9]{2})月([0-9]{2})日" [ area; prefix; suffix ] ->
-        [|int area; int prefix; int suffix|]
+    | Regex @"([0-9]{4})年([0-9]{2})月([0-9]{2})日" [ year; month; day ] ->
+        [|int year; int month; int day|]
     | _ -> [|-1; -1; -1|]
 
+let getComment (node : HtmlNode) =
+    let url = node.Descendants["a"] 
+              |> Seq.map (fun (x:HtmlNode) -> x.Attribute("href").Value())
+              |> Seq.filter (fun x -> x.Contains("/books/"))
+              |> Seq.head
+    // printfn "%s" ("https://elk.bookmeter.com" + url)        
+    let data = HtmlDocument.Load("https://elk.bookmeter.com" + url)
+    let content = data.Descendants("div") 
+                  |> Seq.filter (fun (x:HtmlNode) -> x.HasAttribute("class", "read-book__content"))
+    printfn "---%A---" content
+    if Seq.isEmpty content then "a" 
+        else (Seq.head content).InnerText()               
 let getJson (node : HtmlNode) =
     let getDiv key = node.Descendants["div"]
                      |> Seq.filter(fun (x:HtmlNode) -> x.HasAttribute("class", key))
@@ -29,11 +41,12 @@ let getJson (node : HtmlNode) =
                |> Seq.head |> (fun x -> x.InnerText())           
     let page = getDiv "detail__page"
                |> (fun x -> x.InnerText() |> int)
-    BookJson.Root(date, title, author, page)                        
+    let comment = getComment node           
+    BookJson.Root(date, title, author, page, comment)                        
 
 [<EntryPoint>]
 let main argv =
-    let data = BookHtml.Load("https://elk.bookmeter.com/users/580549/books/read") 
-    let bookList = data.Html.Descendants["div"] |>  Seq.filter (fun (x:HtmlNode) -> x.HasAttribute("class", "book__detail"))
-    do bookList |> Seq.iter (getJson >> printfn "%A")
+    let data = HtmlDocument.Load("https://elk.bookmeter.com/users/580549/books/read") 
+    let bookList = data.Descendants["div"] |>  Seq.filter (fun (x:HtmlNode) -> x.HasAttribute("class", "book__detail"))
+    do bookList |> Seq.head |> (getJson >> printfn "%A")
     0 // return an integer exit code
