07f9653fb3babb8dd7868286716559abce43effb
wkwkes <oshi.hiroki@gmail.com>
2017-03-11 12:17:13 +0900

    make it possible to follow next pages

--- dmscraper/dmscraper.fs
+++ dmscraper/dmscraper.fs
@@ -4,7 +4,7 @@ open FSharp.Data
 open System.Text.RegularExpressions
 
 // type BookHtml = HtmlProvider< "https://elk.bookmeter.com/users/580549/books/read" >
-type BookJson = JsonProvider<""" { "date":[2017, 3, 8], "title":"羆嵐", "author":"吉村 昭", "page":226, "comment":"おもしろかった" } """>
+type BookJson = JsonProvider<""" { "title":"羆嵐", "date":[2017, 3, 8], "author":"吉村 昭", "page":226, "comment":"おもしろかった" } """>
 
 let parseDate date =
     let (|Regex|_|) pattern input =
@@ -16,6 +16,8 @@ let parseDate date =
         [|int year; int month; int day|]
     | _ -> [|-1; -1; -1|]
 
+// span class="content__netabareflag"
+// review__content review__content--netabare"
 let getComment (node : HtmlNode) =
     let url = node.Descendants["a"] 
               |> Seq.map (fun (x:HtmlNode) -> x.Attribute("href").Value())
@@ -26,7 +28,7 @@ let getComment (node : HtmlNode) =
                let data = HtmlDocument.Load("https://elk.bookmeter.com" + url) in
                let content = data.Descendants("div")
                              |> Seq.filter (fun (x:HtmlNode) -> 
-                             x.HasAttribute("class", "review__content review__content--default")) in
+                             x.HasAttribute("class", "review__content review__content--default") || x.HasAttribute("class", "review__content review__content--netabare")) in
                if Seq.isEmpty content then "" 
                else (Seq.head content).InnerText())
 
@@ -47,12 +49,32 @@ let getJson (group : HtmlNode) =
     let page = getDiv "detail__page"
                |> (fun x -> x.InnerText() |> int)
     let comment = getComment group           
-    BookJson.Root(date, title, author, page, comment)                        
+    BookJson.Root(title, date, author, page, comment).ToString()               
+let pprinter p =
+    let rec aux = function
+        | [] -> ()
+        | [x] -> printfn "%s" x
+        | x::xs -> (printfn "%s," x; aux xs)
+    printfn "["; aux p; printfn "]"
+    
+let rec scraper (url : string) old = 
+    let data = HtmlDocument.Load url in
+    let newList = data.Descendants["li"]  // 20 件所得して処理
+                  |>  Seq.filter (fun (x:HtmlNode) -> x.HasAttribute("class", "group__book"))
+                  |> Seq.map getJson
+                  |> Seq.toList
+    let _newUrl = data.Descendants["a"]
+                |> Seq.filter (fun (x : HtmlNode) -> x.HasAttribute("rel", "next"))
+                |> Seq.map (fun (x : HtmlNode) -> x.Attribute("href").Value())
+    match Seq.isEmpty _newUrl with
+    | true -> old @ newList
+    | false -> let newUrl = "https://elk.bookmeter.com" + (Seq.head _newUrl)
+               scraper newUrl (old @ newList)
+        
 
 [<EntryPoint>]
 let main argv =
-    let data = HtmlDocument.Load("https://elk.bookmeter.com/users/580549/books/read")
-    let bookList = data.Descendants["li"] 
-                   |>  Seq.filter (fun (x:HtmlNode) -> x.HasAttribute("class", "group__book"))
-    do bookList |> Seq.iter (getJson >> (printfn "%A,"))
+    let url = "https://elk.bookmeter.com/users/580549/books/read"
+    let bookList = scraper url []
+    do bookList |> pprinter
     0 // return an integer exit code
